149998df663ad5bcead443e8b4308788f9d6a654
Bingshen Wang <xiaobing0000_@126.com>
2016-10-31 17:52:00 +0800

    Update entrypoint.sh
    resolve outing traffic iptables

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ rsyslogd
 sysctl -w net.ipv4.ip_forward=1
 
 # configure firewall
-iptables -t nat -A POSTROUTING -s 172.31.99.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 172.31.99.0/24 -j MASQUERADE
 iptables -A FORWARD -s 172.31.99.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
 
 sed -i "s/username/$USERNAME/g" /etc/ppp/chap-secrets
